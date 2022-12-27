1a1b7e1e3fbfa5a8d99fe82ab650d10fa769ca6a
lutf87 <luthfiarrahman007@gmail.com>
2022-12-28 00:19:38 +0700

    modif auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('home.template')
 
 @section('content')
 <div class="container">
@@ -32,6 +32,20 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="notel" class="col-md-4 col-form-label text-md-end">{{ __('Nomor Telepon') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="notel" type="number" class="form-control @error('name') is-invalid @enderror" name="notel" value="{{ old('notel') }}" required autocomplete="notel" autofocus>
+
+                                @error('notel')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['prefix'=>'/'], function() {
+
+    // index
+    Route::get('', [HomeController::class, 'index'])->name('home');
+
+    // kategori
+    Route::get('/kategori', [HomeController::class, 'kategori'])->name('kategori');
+});
+
